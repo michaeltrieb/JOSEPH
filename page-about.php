39dd3e46f742970b487cf6ed4c6a4343384236d8
Michael Trieb <mikeytrieb@gmail.com>
2017-05-22 21:12:47 -0500

    Added google map api to page-about.php

--- page-about.php
+++ page-about.php
@@ -5,6 +5,16 @@
 
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
+<!-- Google Maps API Key: AIzaSyCntrEQQu7o_mmL4uN9heTGLZEKwVF8kU8 -->
+
+<div id="map">
+    <iframe
+      width="700"
+      height="350"
+      frameborder="0" style="border:0"
+      src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCntrEQQu7o_mmL4uN9heTGLZEKwVF8kU8&q=6225+Delmar,St.Louis,MO" allowfullscreen>
+    </iframe>
+</div><!--map-->
 
 
 
